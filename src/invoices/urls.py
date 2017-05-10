e818f85799b25ad6f69f0a77727bca39e11d0889
Antonio G. Bolaño <gonzalez_antbol@gva.es>
2017-05-10 12:42:27 +0200

    Added schema for API through coreapi

--- src/invoices/urls.py
+++ src/invoices/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from . import views
 
+schema_view = get_schema_view(title='django-invoices API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -12,4 +14,5 @@ router.register(r'entries', views.InvoiceEntryViewSet)
 urlpatterns = [
     # url(r'^$', views.api_root),
     url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
 ]
